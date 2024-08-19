34f66a87bc4b12531a418c3d0b6946c8a0e19443
Jason Li <JasonLiSoftware@gmail.com>
2024-08-19 17:22:25 -0400

    make spacing more responsive

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -24,6 +24,11 @@ export default {
       xl: "1280px",
       "2xl": "1536px",
     },
+    extend: {
+      spacing: {
+        section: "var(--section-spacing)",
+      },
+    },
   },
   plugins: [],
 };
